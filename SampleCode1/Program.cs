3518a20bd563fccf7b258467f83a7b98167b88a0
mikethemerchant <35412436+mikethemerchant@users.noreply.github.com>
2018-03-18 11:48:21 -0400

    upload sampleCode project
    going to upload samples for blog

---
+++ SampleCode1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("do i have this setup right?");
+
+            string response = Console.ReadLine();
+
+            Console.WriteLine("Response: {0}", response);
+
+            Console.ReadKey();
+        }
+    }
+}
